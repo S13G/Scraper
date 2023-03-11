d72d7e9cef8a98f2537e1bdeeebe1d3369b164c7
s13g <ayflix0@gmail.com>
2023-03-11 12:09:18 +0100

    fixed the first occurence

--- CCScraper_2/facebook_scraper.py
+++ CCScraper_2/facebook_scraper.py
@@ -1,4 +1,5 @@
 # Import the necessary libraries
+from itertools import zip_longest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
@@ -12,7 +13,7 @@ import time
 chrome_options = Options()
 
 # uncomment this line if you want the code to run without browser interface
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
 
 # Ask the user for the keywords
@@ -30,8 +31,8 @@ driver.get(url)
 
 # Find the search box and enter the keywords
 # Login to Facebook first
-email = input("Enter your Facebook email: ")
-password = input("Enter your Facebook password: ")
+email = "ayflix0@gmail.com"
+password = "S13gd0ma1n£"
 email_box = driver.find_element(By.ID, "email")
 password_box = driver.find_element(By.ID, "pass")
 email_box.send_keys(email)
@@ -66,31 +67,42 @@ positive_sentences = []
 negative_sentences = []
 other_sentences = []
 
+# Loop through all matching tags and sentences
 for tag in matching_tags:
     for sentence in tag.get_text().split('.'):
-        if any(word.strip().lower() in sentence.strip().lower() for word in positive_words):
-            clean_sentence = sentence.strip().replace('\n', ' ').replace('\t', ' ')
-            clean_sentence = re.sub(r'[^\w\s]', '', clean_sentence)
-            positive_sentences.append(clean_sentence)
-        elif any(word.strip().lower() in sentence.strip().lower() for word in negative_words):
-            clean_sentence = sentence.strip().replace('\n', ' ').replace('\t', ' ')
-            clean_sentence = re.sub(r'[^\w\s]', '', clean_sentence)
-            negative_sentences.append(clean_sentence)
-        else:
-            clean_sentence = sentence.strip().replace('\n', ' ').replace('\t', ' ')
-            clean_sentence = re.sub(r'[^\w\s]', '', clean_sentence)
-            other_sentences.append(clean_sentence)
+        # Convert sentence to lowercase and find the index of the keyword
+        sentence_lower = sentence.strip().lower()
+        keyword_index = sentence_lower.find(keywords.lower())
+        # If the keyword exists in the sentence
+        if keyword_index != -1:
+            # Extract the part of the sentence from the keyword to the end
+            sentence_from_keyword = sentence[keyword_index:]
+            # Check if the sentence contains any positive keywords
+            if any(word.strip().lower() in sentence_from_keyword.strip().lower() for word in positive_words):
+                clean_sentence = sentence_from_keyword.strip().replace('\n', ' ').replace('\t', ' ')
+                clean_sentence = re.sub(r'[^\w\s]', '', clean_sentence)
+                positive_sentences.append(clean_sentence)
+            # Check if the sentence contains any negative keywords
+            elif any(word.strip().lower() in sentence_from_keyword.strip().lower() for word in negative_words):
+                clean_sentence = sentence_from_keyword.strip().replace('\n', ' ').replace('\t', ' ')
+                clean_sentence = re.sub(r'[^\w\s]', '', clean_sentence)
+                negative_sentences.append(clean_sentence)
+            # If the sentence doesn't contain any positive or negative keywords, add it to "other" sentences
+            else:
+                clean_sentence = sentence_from_keyword.strip().replace('\n', ' ').replace('\t', ' ')
+                clean_sentence = re.sub(r'[^\w\s]', '', clean_sentence)
+                other_sentences.append(clean_sentence)
 
 # Create a pandas DataFrame for each category of sentences
-positive_df = pd.DataFrame({'Sentence': positive_sentences})
-negative_df = pd.DataFrame({'Sentence': negative_sentences})
-other_df = pd.DataFrame({'Sentence': other_sentences})
+positive_df = pd.DataFrame({'Positive Keywords': positive_sentences})
+negative_df = pd.DataFrame({'Negative Keywords': negative_sentences})
+other_df = pd.DataFrame({'Other Keywords': other_sentences})
 
-# Write each DataFrame to a separate sheet in an Excel file
+# Concatenate the DataFrames and write to CSV and XLSX files
+df = pd.concat([positive_df, negative_df, other_df], axis=1)
+df.to_csv('results.csv', index=False)
 with pd.ExcelWriter('results.xlsx') as writer:
-    positive_df.to_excel(writer, sheet_name='Positive', index=False)
-    negative_df.to_excel(writer, sheet_name='Negative', index=False)
-    other_df.to_excel(writer, sheet_name='Other', index=False)
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
 
 # Close the Selenium driver
 driver.quit()
